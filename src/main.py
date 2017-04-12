1c3edc1e38bab68a6a9fa8cb00a71b3eb3995360
Wei Xie <xiewei.fire@gmail.com>
2017-04-12 15:06:39 +0800

    __file__ not stable, changed to abspath().
    Signed-off-by: Wei Xie <xiewei.fire@gmail.com>

--- src/main.py
+++ src/main.py
@@ -11,7 +11,8 @@ class adbOperator:
         self.dm = reg()
         self.env = os.environ.copy()
         # print self.env["PATH"]
-        self.rootPath = dirname(dirname(__file__))
+        self.rootPath = os.path.abspath('..')
+        print self.rootPath
         self.phonePath = '/storage/emulated/0/tencent/MicroMsg/WeiXin'
         self.env["PATH"] = os.path.join(
             self.rootPath, 'adb') + ";" + self.env["PATH"]
@@ -25,29 +26,30 @@ class adbOperator:
     def screenshot(self):
         cmd = 'adb -s ' + str(self.deviceId) + \
             ' exec-out screencap -p > ' + os.path.join(
-            self.rootPath,'cache', 'test.png')
-        print cmd
-        subprocess.Popen(cmd, env=self.env, shell=True)
-    def createDir(self):
-        cmd = 'adb -s ' + str(self.deviceId) + \
-            ' shell su -c "mkdir ' +self.phonePath +'"'
+            self.rootPath, 'cache', 'test.png')
         print cmd
         subprocess.Popen(cmd, env=self.env, shell=True)
 
+    def createDir(self):
+        cmd = 'adb -s ' + str(self.deviceId) + \
+            ' shell su -c "mkdir ' + self.phonePath + '"'
+        print cmd
+        subprocess.Popen(cmd, env=self.env, shell=True)
 
     def openScanner(self):
         cmd = 'adb -s ' + str(self.deviceId) + \
-        ' shell su -c "am start -n com.tencent.mm/com.tencent.mm.plugin.scanner.ui.BaseScanUI"'
+            (' shell su -c "am start -n com.tencent.mm/'
+            'com.tencent.mm.plugin.scanner.ui.BaseScanUI"')
         print cmd
         subprocess.Popen(cmd, env=self.env, shell=True)
 
-    def pushPic(self,picFile):
+    def pushPic(self, picFile):
         cmd = 'adb -s ' + str(self.deviceId) + \
-        ' push '+picFile+' '+self.phonePath
+            ' push ' + picFile + ' ' + self.phonePath
         print cmd
         subprocess.Popen(cmd, env=self.env, shell=True)
-
 
 
 op = adbOperator(20510497)
-op.pushPic('d:/microMsg.1491963504562.jpg')
+op.openScanner()
+# op.pushPic('d:/microMsg.1491963504562.jpg')
