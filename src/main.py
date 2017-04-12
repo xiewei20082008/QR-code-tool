5a635ed49a16b6345fb14a0963ecf1284da5259e
Wei Xie <xiewei.fire@gmail.com>
2017-04-12 18:21:52 +0800

    Broadcast after push file to ensure scanner notice the changes.
    Signed-off-by: Wei Xie <xiewei.fire@gmail.com>

--- src/main.py
+++ src/main.py
@@ -12,7 +12,6 @@ class adbOperator:
         self.env = os.environ.copy()
         # print self.env["PATH"]
         self.rootPath = os.path.abspath('..')
-        print self.rootPath
         self.phonePath = '/storage/emulated/0/tencent/MicroMsg/WeiXin'
         self.env["PATH"] = os.path.join(
             self.rootPath, 'adb') + ";" + self.env["PATH"]
@@ -20,36 +19,38 @@ class adbOperator:
     def click(self, x, y):
         cmd = 'adb -s ' + str(self.deviceId) + \
             ' shell input tap ' + str(x) + ' ' + str(y)
-        print cmd
         subprocess.Popen(cmd, env=self.env, shell=True)
 
     def screenshot(self):
         cmd = 'adb -s ' + str(self.deviceId) + \
             ' exec-out screencap -p > ' + os.path.join(
             self.rootPath, 'cache', 'test.png')
-        print cmd
         subprocess.Popen(cmd, env=self.env, shell=True)
 
     def createDir(self):
         cmd = 'adb -s ' + str(self.deviceId) + \
             ' shell su -c "mkdir ' + self.phonePath + '"'
-        print cmd
         subprocess.Popen(cmd, env=self.env, shell=True)
 
     def openScanner(self):
         cmd = 'adb -s ' + str(self.deviceId) + \
             (' shell su -c "am start -n com.tencent.mm/'
-            'com.tencent.mm.plugin.scanner.ui.BaseScanUI"')
-        print cmd
+             'com.tencent.mm.plugin.scanner.ui.BaseScanUI"')
         subprocess.Popen(cmd, env=self.env, shell=True)
 
     def pushPic(self, picFile):
         cmd = 'adb -s ' + str(self.deviceId) + \
             ' push ' + picFile + ' ' + self.phonePath
+        subprocess.Popen(cmd, env=self.env, shell=True)
+
+        cmd = 'adb -s ' + str(self.deviceId) + \
+            (' shell am broadcast -a '
+             'android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file://') + \
+            os.path.join(self.phonePath + os.path.basename(picFile))
         print cmd
         subprocess.Popen(cmd, env=self.env, shell=True)
 
 
 op = adbOperator(20510497)
-op.openScanner()
-# op.pushPic('d:/microMsg.1491963504562.jpg')
+# op.openScanner()
+op.pushPic('d:/1.jpg')
